4e27d8d71e0ad036a72bfac25bdd38a642e8c2b3
Gautam Sharma <gautam@zibtek.in>
2023-08-14 20:04:05 +0530

    ✅ Added constant text of app

---
+++ lib/constant/text.dart
@@ -0,0 +1,7 @@
+class AppText{
+  static const String appName = 'ExerciseMates';
+  static const String apiKey = '98f91a7ff6msh42c392d36eea316p18000ajsn980bf779f30f';
+  static const String apiHost = 'exercisedb.p.rapidapi.com';
+  static const String somethingWentWrongText = 'Something Went Wrong';
+  static const String exerciseDetail = 'Exercise Detail';
+}
