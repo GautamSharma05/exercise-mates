51413fa3919a9afcbbfeef0d26f6209e878e7d7f
Gautam Sharma <gautam@zibtek.in>
2023-08-14 20:03:41 +0530

    ✅ Added colors constant file

---
+++ lib/constant/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static const primaryColor = Color(0xffED7014);
+  static const whiteColor = Color(0xffffffff);
+  static const blackColor = Color(0xff000000);
+}
